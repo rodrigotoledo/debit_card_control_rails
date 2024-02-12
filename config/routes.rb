818638ccf591fc4de403b3e7e4cb584f2c2b2b61
rtoledo_dev <rodrigo@rtoledo.inf.br>
2024-02-12 05:21:36 -0300

    Add debit card records to seeds.rb and update schema.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :debit_cards, only: [:create] do
-      post 'reload', on: :member
-      post 'checkout', on: :member
+      patch 'reload', on: :member
+      patch 'checkout', on: :member
     end
   end
 end
