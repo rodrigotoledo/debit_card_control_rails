9749cf2f3c9155be34e5d47616144a9f4e1333da
rtoledo_dev <rodrigo@rtoledo.inf.br>
2024-02-12 05:21:06 -0300

    Add create, reload, and checkout actions to DebitCardsController and methods to reload_balance and deduct_balance in DebitCard model

--- app/models/debit_card.rb
+++ app/models/debit_card.rb
@@ -1,3 +1,12 @@
 class DebitCard < ApplicationRecord
   validates :number, :balance, presence: true
+  def reload_balance(amount)
+    self.balance += amount
+    save
+  end
+
+  def deduct_balance(amount)
+    self.balance -= amount
+    save
+  end
 end
